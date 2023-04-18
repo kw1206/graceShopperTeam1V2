cfbe2081f224ac77c5b3b829d53a82a693916c0f
Stefani Chang <stefymerayja@gmail.com>
2023-04-18 15:57:54 -0400

    backend: user routes done, products routes started

--- server/api/users.js
+++ server/api/users.js
@@ -15,3 +15,14 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id, {
+      attributes: ['username', 'firstName', 'lastName', 'orderHistory', 'cart']
+    });
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
