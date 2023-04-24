a9f3c86ce65ecae5c22267c67b46fd4f325af44a
ENHouse <elizabeth.house88@gmail.com>
2023-04-24 19:53:47 -0400

    quantity updates on page

--- client/features/cart/CartItem.js
+++ client/features/cart/CartItem.js
@@ -3,46 +3,44 @@ import { useSelector } from 'react-redux';
 import DeleteBtn from './DeleteBtn';
 
 //Need to add these to slices eventually
-function addOne(n) {
-  console.log('quantity is', n, 'user click add one now quant is', n++);
-  return n + 1;
-  // this should dispatch a put request for the item to update it's quantity
-  // the the state that is updated is the quantiy number.
-}
-function subtractOne(n) {
-  let newNum= n - 1;
-  console.log(newNum)
-  return newNum
-}
 
 const CartItem = (props) => {
   // console.log("here is one prop item ", props)
   const { cartItem } = props;
   const { quantity } = cartItem;
   const { product } = cartItem;
-  const [amountValue, setAmountValue] = useState(parseInt(quantity)); // type is string
+  const [amountValue, setAmountValue] = useState(parseInt(quantity)); // type is now number
 
-  console.log(typeof amountValue);
-
-  // console.log("here is product info ", product)
   const { id, title, price, thumbnail } = product;
 
   useEffect(() => {
-    
-    console.log("quantiy change")
+    console.log('quantiy change');
   }, [quantity]);
+
+  //ADDING AND SUBTRACTING FUNCTIONS //
+  function addOne() {
+    const newValue = amountValue + 1;
+    setAmountValue(newValue)
+  }
+
+  function subtractOne() {
+    const newValue = amountValue - 1;
+    setAmountValue(newValue);
+    // return n-1
+  }
+
   return (
     <>
       <span id={id}>
         <h4 className="item-Name">{title}</h4>
         <img className="cart-thumbnail" src={thumbnail} />
         <span>${price}</span>
-        <button className="addOne" onClick={() => addOne(quantity)}>
+        <button className="addOne" onClick={addOne}>
           {' '}
           +{' '}
         </button>{' '}
         {amountValue}{' '}
-        <button className="subtractOne" onClick={() => subtractOne(quantity)}>
+        <button className="subtractOne" onClick={subtractOne}>
           {' '}
           -{' '}
         </button>
