91274f90decbfcbb9ed7275b54e57ed3f9647723
kw1206 <kit.wallace1206@gmail.com>
2023-04-24 16:30:43 -0400

    updated styling of nav, login and signup

--- client/features/auth/SignupForm.js
+++ client/features/auth/SignupForm.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { authenticate } from '../../app/store';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { authenticate } from "../../app/store";
 
 const SignupForm = ({ name, displayName }) => {
   const { error } = useSelector((state) => state.auth);
@@ -13,42 +13,53 @@ const SignupForm = ({ name, displayName }) => {
     const lastName = evt.target.lastName.value;
     const username = evt.target.username.value;
     const password = evt.target.password.value;
-    dispatch(authenticate({ firstName, lastName, username, password, method: formName }));
+    if (!firstName || !lastName || !username || !password) return alert ("Please fill in every field of the register form")
+    dispatch(
+      authenticate({
+        firstName,
+        lastName,
+        username,
+        password,
+        method: formName,
+      })
+    );
   };
 
   return (
-    <div>
-      <h2>Register as new user</h2>
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
-          <label htmlFor="firstName">
-            <small>First Name</small>
-          </label>
-          <input name="firstName" type="text" />
-        </div>
-        <div>
-          <label htmlFor="lastName">
-            <small>Last Name</small>
-          </label>
-          <input name="lastName" type="text" />
-        </div>
-        <div>
-          <label htmlFor="username">
-            <small>Username</small>
-          </label>
-          <input name="username" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password">
-            <small>Password</small>
-          </label>
-          <input name="password" type="password" />
-        </div>
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && <div> {error} </div>}
-      </form>
+    <div className="page">
+      <div id="registerPage">
+        <h2>Register as new user</h2>
+        <form onSubmit={handleSubmit} name={name}>
+          <div>
+            <label htmlFor="firstName">
+              <small>First Name</small>
+            </label>
+            <input required name="firstName" type="text" />
+          </div>
+          <div>
+            <label htmlFor="lastName">
+              <small>Last Name</small>
+            </label>
+            <input required name="lastName" type="text" />
+          </div>
+          <div>
+            <label htmlFor="username">
+              <small>Username</small>
+            </label>
+            <input required name="username" type="text" />
+          </div>
+          <div>
+            <label htmlFor="password">
+              <small>Password</small>
+            </label>
+            <input required name="password" type="password" />
+          </div>
+          <div>
+            <button className="loginSignUpBtn" type="submit">{displayName}</button>
+          </div>
+          {/* {error && <div> {error} </div>} */}
+        </form>
+      </div>
     </div>
   );
 };
